cd31ccd3d010c16f88d3552feedf9dc6714eef54
Rachot Ruengsaengnateetip <rachot.ruengsaengnateetip@gmail.com>
2024-11-10 18:51:00 +0700

    remove# staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-#STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
